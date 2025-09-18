9b11cbd26542123b718ec7ea29a7d8381cf508d1
Andrey-Shelgunov <selgunova3@gmail.ru>
2025-09-18 10:08:59 +0300

    создан массив с перебором чисел с 1 до 10000

--- testarray/testarray/Program.cs
+++ testarray/testarray/Program.cs
@@ -19,7 +19,9 @@ namespace testarray
             Console.WriteLine(array[0]);
             Console.WriteLine(array[1]);
             Console.WriteLine(array[2]);
-            Console.WriteLine("---------------------");
+
+            Console.WriteLine("-----------------------------------------------------------");
+
             string[] twoarray = new string[3];
             twoarray[0] = array[2];
             twoarray[1] = array[1];
@@ -27,6 +29,20 @@ namespace testarray
             Console.WriteLine(twoarray[0]);
             Console.WriteLine(twoarray[1]);
             Console.WriteLine(twoarray[2]);
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            int[] tenarray = new int[10000];
+            for (int a = 0; a < tenarray.Length; a++)
+            {
+                tenarray[a] = a + 1;
+            }
+            Console.Write("[");
+            for (int a = 0; a < tenarray.Length-1; a++)
+            {
+                Console.Write(tenarray[a] + ",");
+            }
+            Console.Write(tenarray[tenarray.Length - 1] + "]");
         }
     }
 }
