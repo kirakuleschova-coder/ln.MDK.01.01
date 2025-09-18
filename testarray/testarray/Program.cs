20c4e81bdc160f7ada028da32a0882c7aad96c40
Andrey-Shelgunov <selgunova3@gmail.ru>
2025-09-18 09:07:12 +0300

    Поменяли второй элемент массива

---
+++ testarray/testarray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[3];
+            array[0] = "Мультимедийные технологии";
+            array[1] = "Физическая культура";
+            array[2] = "Менеджмент в профессиональной деятельности";
+            array[1] = "Физра";
+        }
+    }
+}
