3915df50c93efdce47c43d4f8c55848529a21513
Andrey-Shelgunov <selgunova3@gmail.ru>
2025-09-18 08:38:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ln.MDK._01._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
