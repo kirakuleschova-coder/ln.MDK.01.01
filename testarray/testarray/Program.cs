e8de199872f575b5c9444433826f4b21f21e2ee5
Andrey-Shelgunov <selgunova3@gmail.ru>
2025-09-18 09:14:53 +0300

    создали новый массив и перевернули в нем значения старого массива

--- testarray/testarray/Program.cs
+++ testarray/testarray/Program.cs
@@ -14,7 +14,19 @@ namespace testarray
             array[0] = "Мультимедийные технологии";
             array[1] = "Физическая культура";
             array[2] = "Менеджмент в профессиональной деятельности";
+
             array[1] = "Физра";
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+
+            string[] twoarray = new string[3];
+            twoarray[0] = array[2];
+            twoarray[1] = array[1];
+            twoarray[2] = array[0];
+            Console.WriteLine(twoarray[2]);
+            Console.WriteLine(twoarray[1]);
+            Console.WriteLine(twoarray[0]);
         }
     }
 }
