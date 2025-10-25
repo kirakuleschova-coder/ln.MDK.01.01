51f97eb84edf894694d29818d0dd021ccc8f72f3
Andrey-Shelgunov <selgunova3@gmail.ru>
2025-10-25 10:09:05 +0300

    Создала запрос, который выводит день недели  в зависимости от его номера

--- Домашняя работа/Домашняя работа/Program.cs
+++ Домашняя работа/Домашняя работа/Program.cs
@@ -20,7 +20,25 @@ namespace Домашняя_работа
         }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите номер дня недели (1-7): ");
+            int DayWeek;
+            if (int.TryParse(Console.ReadLine(), out DayWeek))
+            {
+                if (DayWeek >= 1 && DayWeek <= 7)
+                {
+                    DayOfWeek day = (DayOfWeek)DayWeek;
+                    Console.WriteLine($"Это {day}");
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный номер. Введите число от 1 до 7.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный номер. Введите число от 1 до 7.");
+            }
+            Console.ReadLine();
         }
     }
 }
