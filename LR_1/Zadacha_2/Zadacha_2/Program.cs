c5e04570836c657eec33e7312150ff7ef8c649f4
Кира <Kirakuleschova@gmail.com>
2025-11-10 20:16:40 +0300

    Генерирует случайное число в диапазоне [10, 9999] (включительно от 10, исключая 10000)

---
+++ LR_1/Zadacha_2/Zadacha_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3000];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10, 10000);
+            }
+        }
+    }
+}
